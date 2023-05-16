7cc2a2fe64135ac36542e80499245e2634ddded3
JayaS3011 <124575398+JayaS3011@users.noreply.github.com>
2023-05-16 12:06:58 +0200

    code changes

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
 console.log("running");
-const forms = document.getElementById("my-forms");
+
+let todoList = [];
+const form = document.getElementById("my-forms");
 const inputText = document.getElementById("input-text");
 const thingsToDo = document.getElementById("thingstodo");
 const addTodoButton = document.getElementById("add-btn");
 
-function handleForm() {
-  console.log("form submit", inputText.value);
-  addToDo(inputText.value);
-}
+function handleForm(event) {
+  event.preventDefault(); // it prevents from refreshing the page before submitting the data.
+  console.log("form submit" + inputText.value);
 
+  if (inputText.value.length < 4) {
+    //if conditions throws alert message when length of the value lessthen 4
+    window.alert("give more descriptive");
+    inputText.value = ""; //refresh the input
+    return;
+  }
+  addToDo(inputText.value);
+  inputText.value = "";
+}
+//In addtoDoTo() function i created html elements div,p,button and appending it into things to do container and calling removeToDo() function
 function addToDo(text) {
+  const toDoId = Date.now();
+  todoList.push({
+    key: toDoId,
+    todoitem: text,
+  });
+
+  console.log(todoList);
+
   const itemContainer = document.createElement("div");
   itemContainer.style.display = "flex";
 
   const paragraph = document.createElement("p");
   paragraph.textContent = text;
 
-  const removebutton = document.createElement("button");
-  removebutton.textContent = "Remove item from to do";
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "Remove item from to do";
 
-  itemContainer.append(paragraph, removebutton);
+  removeButton.addEventListener("click", () =>
+    removeToDo(itemContainer, toDoId)
+  );
+
+  itemContainer.append(paragraph, removeButton);
   thingsToDo.appendChild(itemContainer);
 }
+// this function removing to do items from the list
+function removeToDo(removingdata, toDoId) {
+  removingdata.remove();
+}
 
-function removeToDo() {}
-
-//forms.addEventListener("submit", handleForm);
-addTodoButton.addEventListener("click", () => handleForm);
+form.addEventListener("submit", handleForm);
+addTodoButton.addEventListener("click", handleForm);
+//form.addEventListener("submit", (event) => event.preventDefault());
