0d4a81db429d3aa83f3772f4c9d26c67983f9dc2
Jayakodehode <jaya.kodehode@gmail.com>
2023-05-24 09:31:40 +0200

    added code for displaying data in browser and added style.css

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const form = document.getElementById("my-forms");
 const inputText = document.getElementById("input-text");
 const thingsToDo = document.getElementById("thingstodo");
 const addTodoButton = document.getElementById("add-btn");
-//showData();
+//const container = document.getElementsByClassName("container");
+displayData();
 
 function handleForm(event) {
   event.preventDefault(); // it prevents from refreshing the page before submitting the data.
@@ -37,6 +38,7 @@ function addToDo(text) {
 
   const paragraph = document.createElement("p");
   paragraph.textContent = text;
+  // paragraph.classList.add("para");
 
   const removeButton = document.createElement("button");
   removeButton.textContent = "Delete";
@@ -47,6 +49,7 @@ function addToDo(text) {
 
   itemContainer.append(paragraph, removeButton);
   thingsToDo.appendChild(itemContainer);
+  // container.append(thingsToDo);
   inputText.value = "";
   saveData();
 }
@@ -68,6 +71,31 @@ function saveData() {
   localStorage.setItem("data", JSON.stringify(todoList));
 }
 
-/*function showData() {
-  thingsToDo.textContent = localStorage.getItem("data");
-}*/
+function displayData() {
+  let todo = localStorage.getItem("data");
+  if (todo === null) {
+    todoList = [];
+  } else {
+    todoList = JSON.parse(todo);
+  }
+  thingsToDo.innerHTML = ""; // Clear the existing list before displaying
+
+  todoList.forEach((todoItem) => {
+    const itemContainer = document.createElement("div");
+    itemContainer.style.display = "flex";
+
+    const paragraph = document.createElement("p");
+    paragraph.textContent = todoItem.todoitem;
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Delete";
+
+    removeButton.addEventListener("click", () =>
+      removeToDo(itemContainer, todoItem.key)
+    );
+
+    itemContainer.append(paragraph, removeButton);
+    thingsToDo.appendChild(itemContainer);
+    // container.append(thingsToDo);
+  });
+}
